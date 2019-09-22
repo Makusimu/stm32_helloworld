da9d71dc5b7983382e41ad5aba51849daa45022b
Makusimu <mail@makusimu.ru>
2019-09-22 03:11:28 +0300

    Blink

--- main.c
+++ main.c
@@ -1,42 +1,72 @@
 #include <stdint.h>
+#include "stm32f1xx.h"
 
 // LED: PC13
+
+GPIO_TypeDef *port = GPIOC;
 
 void delay(uint32_t ticks);
 
 void init_rcc();
 void init_led();
 void toggle_led();
+void led_on();
+void led_off();
 
-void main()
+typedef enum eLedState
+{
+  ON, OFF
+} LedState;
+
+LedState ledState_ = OFF;
+
+int main()
 {
   init_rcc();
   init_led();
   while(1)
   {
     toggle_led();
-    delay(10000);
+    delay(100000);
   }
+  return 0;
 }
 
 void init_rcc()
 {
-
+  SET_BIT(RCC->APB2ENR, RCC_APB2ENR_IOPCEN);
 }
 
 void init_led()
 {
-
+  CLEAR_BIT(port->CRH, GPIO_CRH_MODE13|GPIO_CRH_CNF13);
+  SET_BIT(port->CRH, GPIO_CRH_MODE13_0);
 }
 
 void toggle_led()
 {
+  if(ledState_ == ON)
+    led_off();
+  else
+    led_on();
+}
 
+void led_on()
+{
+  SET_BIT(port->ODR, GPIO_ODR_ODR13);
+
+  ledState_ = ON;
+}
+
+void led_off()
+{
+  CLEAR_BIT(port->ODR, GPIO_ODR_ODR13);
+  ledState_ = OFF;
 }
 
 void delay(uint32_t ticks)
 {
-  if(ticks <= 0)
+  if(ticks == 0)
     return;
 
   volatile uint32_t cnt = ticks;
